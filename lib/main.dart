0b2a71e7194f436f9e4e01eb685aefa580ba0f88
discrilkx379 <discrilkx379@gmail.com>
2024-06-10 06:47:38 +0000

    b

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:myapp/list_screen.dart';
+import 'package:database/list_screen.dart';
 
 void main() {
   runApp(const MyApp());
